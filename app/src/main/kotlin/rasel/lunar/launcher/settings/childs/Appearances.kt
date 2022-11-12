d9d80673b247e339a857931fba1e2f3886eb4ef4
iamrasel <mdraselhossain2312@gmail.com>
2022-11-12 18:36:14 +0600

    Add option to change wallpaper

--- app/src/main/kotlin/rasel/lunar/launcher/settings/childs/Appearances.kt
+++ app/src/main/kotlin/rasel/lunar/launcher/settings/childs/Appearances.kt
@@ -18,14 +18,24 @@
 
 package rasel.lunar.launcher.settings.childs
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
+import android.app.WallpaperManager
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.graphics.Matrix
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -38,6 +48,7 @@ import rasel.lunar.launcher.helpers.ColorPicker
 import rasel.lunar.launcher.helpers.Constants
 import rasel.lunar.launcher.helpers.UniUtils
 import rasel.lunar.launcher.settings.PrefsUtil
+import java.io.IOException
 import java.util.*
 
 
@@ -79,6 +90,7 @@ internal class Appearances : BottomSheetDialogFragment() {
         }
 
         binding.background.setOnClickListener { selectBackground() }
+        binding.changeWallpaper.setOnClickListener { selectWallpaper() }
     }
 
     override fun onResume() {
@@ -113,8 +125,52 @@ internal class Appearances : BottomSheetDialogFragment() {
         }
     }
 
+    private fun selectWallpaper() {
+        if (requireActivity().checkSelfPermission(READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requireActivity().requestPermissions(arrayOf(READ_EXTERNAL_STORAGE), 1)
+        } else {
+            wallpaperChangeLauncher.launch(Intent(Intent.ACTION_PICK).setType("image/*"))
+        }
+    }
+
     private fun defaultColorString() =
         requireActivity().getString(UniUtils().getColorResId(
             requireContext(), android.R.attr.colorBackground)).replace("#", "")
+
+    private var wallpaperChangeLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            try {
+                val uri = result.data?.data
+                val projection = arrayOf(MediaStore.Images.Media.DATA)
+                val cursor = requireContext().contentResolver.query(
+                    uri!!, projection, null, null, null
+                )
+                cursor?.moveToFirst()
+                val index = cursor!!.getColumnIndex(projection[0])
+                val filePath = cursor.getString(index)
+                cursor.close()
+                val bitmap = BitmapFactory.decodeFile(filePath)
+                val matrix = Matrix()
+                matrix.postRotate(0F)
+                try {
+                    if (bitmap != null) {
+                        WallpaperManager.getInstance(requireContext()).setBitmap(bitmap)
+                        Toast.makeText(requireContext(),
+                            requireActivity().getString(R.string.wallpaper_change_success), Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(requireContext(),
+                            requireActivity().getString(R.string.image_pick_failed), Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(requireContext(),
+                        requireActivity().getString(R.string.something_went_wrong), Toast.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 
 }
